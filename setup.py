b0cd9f3602d7e670827af2ac31d5207267489fc2
gousaiyang <gousaiyang@163.com>
2018-04-19 23:46:55 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='colorlabels',
+    version='0.1',
+    description='Provides awesome colorful labels in console. Designed for message display and interaction in automated scripts.',
+    author = 'Saiyang Gou',
+    author_email = 'gousaiyang223@gmail.com',
+    url='https://github.com/gousaiyang/colorlabels',
+    py_modules=['colorlabels'],
+    install_requires=['colorama'],
+)
